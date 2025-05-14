4d438a9f7fa79dfca0c70be8c4c6d08ded5e7804
Yamin14 <syalikazmi@gmail.com>
2025-05-14 08:54:09 -0700

    vercel.json

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: "https://amn-finance-manager.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
 }));
 app.options('*', cors());
 app.use(express.json());
