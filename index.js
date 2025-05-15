fd486755dfd052318431872bac10ab4fe7775754
Yamin14 <syalikazmi@gmail.com>
2025-05-15 06:39:04 -0700

    vercel.json

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const authRoutes = require('./routes/auth');
 const authMiddleware = require('./middleware/authMiddleware');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 // Middleware
 app.use(cors({
